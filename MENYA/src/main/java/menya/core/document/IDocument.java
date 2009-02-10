6fe0302d2d21eae17b5fdd2124cd72b65c980414
berger.max <berger.max@localhost>
2009-02-10 13:12:59 +0000

    Fixed checkstyle issues

--- MENYA/src/main/java/menya/core/document/IDocument.java
+++ MENYA/src/main/java/menya/core/document/IDocument.java
@@ -4,14 +4,16 @@ import menya.core.background.IBackground;
 
 /**
  * This interfaces represents a MENYA annotation document.
+ * 
  * @author Dominik
- *
+ * @version $Revision$
  */
 public interface IDocument {
-	
-	/**
-	 * retrieves the background of this document.
-	 * @return the background of this document
-	 */
-	public IBackground getBackground();
+
+    /**
+     * retrieves the background of this document.
+     * 
+     * @return the background of this document
+     */
+    IBackground getBackground();
 }
