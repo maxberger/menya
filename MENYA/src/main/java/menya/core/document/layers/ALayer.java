7237c14c4695a41f152875ceaff119d4ba725e40
berger max <berger.max@gmail.com>
2009-03-06 10:48:52 +0000

    style fixes

--- MENYA/src/main/java/menya/core/document/layers/ALayer.java
+++ MENYA/src/main/java/menya/core/document/layers/ALayer.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2009 - 2009 by Menya Project
-
+ * 
  * This file is part of Menya.
-
+ * 
  * Menya is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
-
+ * 
  * Menya is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
-
+ * 
  * You should have received a copy of the GNU Lesser Public License along with
  * Menya. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,9 +23,10 @@ package menya.core.document.layers;
 import menya.core.document.ILayer;
 
 /**
- * is the abstract super classa of all layers.
+ * is the abstract super class of all layers.
+ * 
  * @author Dominik
- * @version $ Revision: 19 $
+ * @version $Revision$
  */
 public abstract class ALayer implements ILayer {
 
