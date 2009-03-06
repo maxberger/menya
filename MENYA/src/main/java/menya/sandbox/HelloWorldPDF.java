2b824f7430bcbf70e73e83d90a476d9a5e4a086d
berger max <berger.max@gmail.com>
2009-03-06 11:13:32 +0000

    use cm; set media box; open created PDF

--- MENYA/src/main/java/menya/sandbox/HelloWorldPDF.java
+++ MENYA/src/main/java/menya/sandbox/HelloWorldPDF.java
@@ -1,59 +1,78 @@
 package menya.sandbox;
 
+import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
 
 import org.pdfbox.exceptions.COSVisitorException;
 import org.pdfbox.pdmodel.PDDocument;
 import org.pdfbox.pdmodel.PDPage;
+import org.pdfbox.pdmodel.common.PDRectangle;
 import org.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.pdfbox.pdmodel.font.PDType1Font;
 
 public class HelloWorldPDF {
 
-	public void go() {
+    private static final float CM_TO_INCH = 1.0f / 2.54f;
+    private static final float INCH_TO_PT = 72f;
+    private static final float CM_TO_PT = HelloWorldPDF.CM_TO_INCH
+            * HelloWorldPDF.INCH_TO_PT;
 
-		String file = "/tmp/test.pdf";
-		String message = "Hello, World!";
+    public void go() {
 
-		PDDocument doc = null;
-		try {
-			doc = new PDDocument();
-			PDPage page = new PDPage();
-			doc.addPage(page);
-			org.pdfbox.pdmodel.font.PDFont font = PDType1Font.HELVETICA_BOLD;
-			PDPageContentStream contentStream = new PDPageContentStream(doc,
-					page);
-			contentStream.beginText();
-			contentStream.setFont(font, 12F);
-			contentStream.moveTextPositionByAmount(100F, 700F);
-			contentStream.drawString(message);
-			contentStream.endText();
-			
-			contentStream.fillRect(72F, 72F, 72F,72F);
-			
-			//contentStream.drawLine(144f,144f,288f,288f);
-			
-			contentStream.close();
-			doc.save(file);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (COSVisitorException e) {
-			e.printStackTrace();
-		} finally {
-			if (doc != null)
-				try {
-					doc.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		}
-	}
+        final String file = "/tmp/test.pdf";
+        final String message = "Hello, World!";
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new HelloWorldPDF().go();
-	}
+        PDDocument doc = null;
+        try {
+            doc = new PDDocument();
+            final PDPage page = new PDPage();
+            page.setMediaBox(new PDRectangle(10.0f * HelloWorldPDF.CM_TO_PT,
+                    10.0f * HelloWorldPDF.CM_TO_PT));
+            doc.addPage(page);
+            final org.pdfbox.pdmodel.font.PDFont font = PDType1Font.HELVETICA_BOLD;
+            final PDPageContentStream contentStream = new PDPageContentStream(
+                    doc, page);
+            contentStream.beginText();
+            contentStream.setFont(font, 12F);
+            contentStream.moveTextPositionByAmount(2 * HelloWorldPDF.CM_TO_PT,
+                    7 * HelloWorldPDF.CM_TO_PT);
+            contentStream.drawString(message);
+            contentStream.endText();
+
+            contentStream.fillRect(1 * HelloWorldPDF.CM_TO_PT,
+                    1 * HelloWorldPDF.CM_TO_PT, 1 * HelloWorldPDF.CM_TO_PT,
+                    1 * HelloWorldPDF.CM_TO_PT);
+
+            // contentStream.drawLine(144f,144f,288f,288f);
+
+            contentStream.close();
+            doc.save(file);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        } catch (final COSVisitorException e) {
+            e.printStackTrace();
+        } finally {
+            if (doc != null) {
+                try {
+                    doc.close();
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            Desktop.getDesktop().open(new File(file));
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        new HelloWorldPDF().go();
+    }
 
 }
