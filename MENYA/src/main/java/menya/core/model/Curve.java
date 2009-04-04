e11d914801cea9180b6a2297ef2a225f760d352a
berger max <berger.max@gmail.com>
2009-04-04 17:52:06 +0000

    style fixes

--- MENYA/src/main/java/menya/core/model/Curve.java
+++ MENYA/src/main/java/menya/core/model/Curve.java
@@ -1,26 +1,28 @@
 /*
  * Copyright 2009 - 2009 by Menya Project
-
+ * 
  * This file is part of Menya.
-
+ * 
  * Menya is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
-
+ * 
  * Menya is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
-
+ * 
  * You should have received a copy of the GNU Lesser Public License along with
  * Menya. If not, see <http://www.gnu.org/licenses/>.
  */
 package menya.core.model;
 
 /**
- * implements the necessary functionality to be a drawed curve within
- * a document.
+ * implements the necessary functionality to be a drawed curve within a
+ * document.
+ * 
  * @author Dominik
+ * @version $Revision: 11 $
  */
 public class Curve implements GraphicalData {
 
