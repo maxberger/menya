ca74a0ff051cfea8f82132308e7a4e2b063144aa
dominik <dominik@localhost>
2009-08-06 00:01:06 +0200

    fixed all headers to be GPL3 (no longer LGPL3)

--- src/main/java/menya/core/document/ILayer.java
+++ src/main/java/menya/core/document/ILayer.java
@@ -1,18 +1,16 @@
 /*
- * Copyright 2009 - 2009 by Menya Project
- * 
  * This file is part of Menya.
  * 
  * Menya is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser Public License as published by the Free Software
+ * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  * 
  * Menya is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser Public License along with
+ * You should have received a copy of the GNU General Public License along with
  * Menya. If not, see <http://www.gnu.org/licenses/>.
  */
 
